a5aa2fce2b8c733ad0fac59056255c128149fb48
Walter Rojas Flores <wrojasf@ejercito.mil.pe>
2024-05-26 22:58:40 -0500

    se avanzo

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,8 @@
 import swal from 'sweetalert2'
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PerfilService } from 'src/app/_service/perfil.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -11,11 +13,10 @@ export class LoginComponent implements OnInit {
 
   username: string;
   password: string;
-  message: string;
-  error: string;
+  hide : boolean = true;
 
   constructor(
-    
+    private perfilService: PerfilService,
     private router: Router
   ) { }
 
@@ -23,7 +24,17 @@ export class LoginComponent implements OnInit {
   }
 
   login(){
-    this.router.navigate(['/principal']);
+    // debugger;
+    // this.perfilService.loginActiveDirectory(this.username,this.password).subscribe((response:any)=> {
+    //   if(response.httpStatus=='OK'){
+        this.router.navigate(['/principal']);
+    //     Swal.fire(response.message, 'Bienvenido al sistema usuario', 'success');
+    //   }else {
+    //     Swal.fire('LO SENTIMOS', 'USUARIO NO EXISTE EN ACTIVE DIRECTORY', 'info');
+    //   }
+    // }, error => {
+    //   Swal.fire('LO SENTIMOS', 'SE PRESENTO UN INCONVENINTE EN LA VALIDACION DEL USUARIO', 'info');
+    // })
     
     
    
