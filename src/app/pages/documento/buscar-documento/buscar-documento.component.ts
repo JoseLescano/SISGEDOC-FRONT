e43a218438d15a33e675fa79631afb1c904e4493
Walter Alonso Rojas Flores <walteralrojasflores@gmail.com>
2025-07-16 16:45:06 -0500

    se avanzo

--- src/app/pages/documento/buscar-documento/buscar-documento.component.ts
+++ src/app/pages/documento/buscar-documento/buscar-documento.component.ts
@@ -132,7 +132,7 @@ export class BuscarDocumentoComponent implements OnInit, AfterViewInit {
         sessionStorage.getItem(environment.codigoOrganizacion),'', 0,20,'codigo', 'desc',
         environment.convertDateToStr(this.range.value['start']),
         environment.convertDateToStr(this.range.value['end'])).subscribe((data: any) => {
-        this.createTable(data);
+        this.createTable(data.content);
         this.cargando = false;
       }, (error: any)=> {
         this.cargando = false;
